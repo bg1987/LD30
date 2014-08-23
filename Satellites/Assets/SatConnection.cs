1511cd8aacfc488cd31129d4a0b7d4c080751d87
bg1987 <bg1987@gmail.com>
2014-08-23 13:34:54 -0400

    Added satellite data transmition (they know who they talked to)

--- Satellites/Assets/SatConnection.cs
+++ Satellites/Assets/SatConnection.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SatConnection : MonoBehaviour {
 
     public Transform startPosition;
     public Transform endPosition;
-    public bool connected;
+    public int myID;
+    public HashSet<int> connectedSats;
 	// Use this for initialization
 	void Start () {
-	    
+        connectedSats = new HashSet<int>();
+        connectedSats.Add(myID);
+	    //maybe some code that will make sure no satelites have the same ID.
 	}
 	
 
@@ -16,10 +20,39 @@ public class SatConnection : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Debug.DrawLine(startPosition.position, endPosition.position,Color.white);
-        connected = Physics2D.Linecast(startPosition.position, endPosition.position,LayerMask.GetMask("Satellite")); //8 = satellite mask
-        if (connected)
-        {
-            Debug.Log("PING!");
+        RaycastHit2D rcHit = Physics2D.Linecast(startPosition.position, endPosition.position,LayerMask.GetMask("Satellite")); //8 = satellite mask
+        if(rcHit.collider == null){
+            return;
         }
+        Debug.Log("Ping!");
+        SatConnection otherSate = rcHit.transform.gameObject.GetComponent<SatConnection>();
+        if (otherSate)
+        {
+            connectedSats = otherSate.Connected(myID,connectedSats);
+            printConnenctedSats();
+        }
+        
 	}
+
+    public HashSet<int> Connected(int otherID, HashSet<int> connected)
+    {
+        Debug.Log(string.Format("Sat {0} and {1} said HI!",myID,otherID));
+        
+        connectedSats.UnionWith(connected);
+        printConnenctedSats();
+
+        return connectedSats;
+
+    }
+
+    public void printConnenctedSats()
+    {
+        string tmp = "";
+        foreach (int id in connectedSats)
+        {
+            tmp += id + " ";
+        }
+        Debug.Log(string.Format("Sat{0}, has talked to the following sats: {1}", myID, tmp));
+    }
 }
+
