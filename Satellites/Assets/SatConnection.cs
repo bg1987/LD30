0f8d4db611051012c3a8bab9d5ceaf3e3e6fdfe0
bg1987 <bg1987@gmail.com>
2014-08-23 01:07:32 -0400

    Inital commit, Satellites game. Orbit system working. Satellites see each other (raycasting) ugly pink trace render (to be replaced with pretty dotted line renderer)

---
+++ Satellites/Assets/SatConnection.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SatConnection : MonoBehaviour {
+
+    public Transform startPosition;
+    public Transform endPosition;
+    public bool connected;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawLine(startPosition.position, endPosition.position,Color.white);
+        connected = Physics2D.Linecast(startPosition.position, endPosition.position,LayerMask.GetMask("Satellite")); //8 = satellite mask
+        if (connected)
+        {
+            Debug.Log("PING!");
+        }
+	}
+}
