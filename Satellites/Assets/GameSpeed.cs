25b0151e115239ecf26ed3e42d8913e379459e8d
bg1987 <bg1987@gmail.com>
2014-08-24 16:53:19 -0400

    More UI. GameSpeed slider

---
+++ Satellites/Assets/GameSpeed.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSpeed : MonoBehaviour {
+    public UnityEngine.UI.Text speedText;
+	
+
+    public void SetSpeed(float speed)
+    {
+        speedText.text = speed.ToString();
+        Time.timeScale = speed;
+    }
+}
