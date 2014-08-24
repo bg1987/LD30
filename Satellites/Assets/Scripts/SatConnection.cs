0a520d8aa804de9cba93c40de994071e3ffce50c
Jake Laurendeau <Jakellaurendeau@gmail.com>
2014-08-24 13:11:14 -0400

    Added in-game line rendering

--- Satellites/Assets/Scripts/SatConnection.cs
+++ Satellites/Assets/Scripts/SatConnection.cs
@@ -10,23 +10,15 @@ public class SatConnection : MonoBehaviour {
     public float radius = 10;
     public int myID;
     public HashSet<int> connectedSats;
+	public static List<LineRenderer> lines = new List<LineRenderer>(1);
+	public static int currentLinerenderer = 0;
 
     void Awake()
     {
         myID = ID_GENERATOR;
         ID_GENERATOR++;
     }
-    
-    
-
-	// Use this for initialization
-	void Start () {
-        connectedSats = new HashSet<int>();
-        connectedSats.Add(myID);
-	    //maybe some code that will make sure no satelites have the same ID.
-	}
-	
-
+   
 
 	// Update is called once per frame
 	void Update () {
@@ -55,13 +47,13 @@ public class SatConnection : MonoBehaviour {
         
 	}
 
-
     //Checks for all the satelites in a radius and makes sure they are in an unbostructed line
     //Then calls recrusively on all new connected (the ones that arent in connectedSats)
     public HashSet<int> CheckConnection(HashSet<int> preConnectedSats)
     {
         RaycastHit2D[] satsInRange = Physics2D.CircleCastAll(transform.position, radius, Vector2.up, 0.1f, LayerMask.GetMask("Satellite"));
         preConnectedSats.Add(myID);
+
         foreach (RaycastHit2D sat in satsInRange)
         {
             SatConnection satCon = sat.transform.gameObject.GetComponent<SatConnection>();
@@ -73,15 +65,91 @@ public class SatConnection : MonoBehaviour {
             //if there is a direct line between the satelites
             if (!Physics2D.Linecast(transform.position,sat.transform.position,LayerMask.GetMask("Planet")))
             {
-                Debug.DrawLine(transform.position, sat.transform.position, Color.green);
+                //Debug.DrawLine(transform.position, sat.transform.position, Color.green);
+				if(currentLinerenderer >= lines.Count)
+				{
+					LineRenderer exampleLine = ((LineRenderer)transform.parent.GetComponent("LineRenderer"));
+					LineRenderer newLine = new GameObject().AddComponent("LineRenderer") as LineRenderer;
+					newLine.sortingLayerName = "Orbit";
+					newLine.name = "LineConnector";
+					newLine.SetWidth(0.2f, 0.2f);
+					newLine.material = exampleLine.material;					
+					Color colors = Color.green;
+					newLine.SetColors(colors, colors);
+					newLine.SetVertexCount(2);
+					lines.Add(newLine);
+				}
+
+				//Debug.Log(currentLinerenderer);
+
+
+				LineRenderer lRend = lines[currentLinerenderer];
+				lRend.enabled = true;
+				lRend.SetPosition(0, transform.position);
+				lRend.SetPosition(1, sat.transform.position);
+				lRend.transform.parent = transform.parent.transform;
+
                 preConnectedSats.Add(satCon.myID);
                 preConnectedSats.UnionWith(satCon.CheckConnection(preConnectedSats));
+				currentLinerenderer++;
             }
-
         }
+
         connectedSats = preConnectedSats;
         return preConnectedSats;
     }
+
+	IEnumerator Start() {
+		connectedSats = new HashSet<int>();
+		connectedSats.Add(myID);
+		while (true) 
+		{
+			yield return new WaitForEndOfFrame ();
+			foreach(LineRenderer rend in lines) {
+					rend.enabled = false;
+			}
+			currentLinerenderer = 0;
+		}
+	}
+	/*
+	public void OnDrawGizmos()
+	{
+			//yield return new WaitForEndOfFrame();
+			
+			Debug.Log("Draw numer");
+
+			Gizmos.DrawLine (transform.position, new Vector3(0, 0, 0));
+			//Gizmos.
+
+
+			GL.PushMatrix();
+			
+			GL.Begin( GL.LINES );
+
+
+
+			foreach(int id in connectedSats)
+			{
+
+				//Transform sat = transform.Find("Sat_" + id);
+
+				
+				//float dist = Vector3.Distance(transform.position, sat.position);
+				
+				GL.Color(new Color(0,1,0,1) );
+				GL.Vertex(transform.position);
+				GL.Color(new Color(1 / 100,1,0,1) );
+				GL.Vertex3(0, 0, 0);
+
+			}
+
+			
+			GL.End();
+			
+			GL.PopMatrix ();
+
+	}
+	*/
 
 
     public HashSet<int> Connected(int otherID, HashSet<int> connected)
