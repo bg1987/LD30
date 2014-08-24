e5b2af8961f6841f1e75a30259ecfff1bae1d287
bg1987 <bg1987@gmail.com>
2014-08-24 19:50:10 -0400

    Added ingame menu functionality.

---
+++ Satellites/Assets/EnableObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableObject : MonoBehaviour {
+
+    public GameObject obj;
+
+    public void EnableObj()
+    {
+        obj.SetActive(true);
+    }
+
+}
