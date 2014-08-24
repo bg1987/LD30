d14ed064bad72694610cdeb69869b220717cd8c4
bg1987 <bg1987@gmail.com>
2014-08-23 21:02:55 -0400

    Moved all scripts and scenes to their respective folders Added a main menu

---
+++ Satellites/Assets/Scripts/MainMenuFunctions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuFunctions : MonoBehaviour {
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(string Name)
+    {
+        Application.LoadLevel(name);
+    }
+
+    public void LoadScene(int id)
+    {
+        Application.LoadLevel(id);
+    }
+}
